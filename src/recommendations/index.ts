315e5e3ee5339a339a1b21e0f5b7b3a7ea1a42d1
Ben Teichman <ben.l.teichman@gmail.com>
2017-07-28 14:25:13 -0400

    feat: flesh out recommendations component to support products (#1)

--- src/recommendations/index.ts
+++ src/recommendations/index.ts
@@ -1,13 +1,31 @@
-import { tag, Tag } from '@storefront/core';
+import { alias, tag, Events, ProductTransformer, Selectors, Store, Structure, Tag } from '@storefront/core';
 
-@tag('gb-recommendations', require('./index.html'), require('./index.css'))
-class Recommendations {}
+@alias('recommendations')
+@tag('gb-recommendations', require('./index.html'))
+class Recommendations {
 
-interface Recommendations extends Tag<Recommendations.Props, Recommendations.State> {}
+  structure: Structure = this.config.structure;
+  state: Recommendations.State = {
+    products: this.mapProducts(Selectors.recommendationsProducts(this.flux.store.getState()))
+  };
+
+  init() {
+    this.flux.on(Events.RECOMMENDATIONS_PRODUCTS_UPDATED, this.updateProducts);
+  }
+
+  updateProducts = (products: Store.Product[]) =>
+    this.set({ products: this.mapProducts(products) })
+
+  mapProducts(products: Store.Product[]) {
+    return products.map(ProductTransformer.transformer(this.structure));
+  }
+}
+
+interface Recommendations extends Tag<any, Recommendations.State> { }
 namespace Recommendations {
-  export interface Props {}
-
-  export interface State {}
+  export interface State {
+    products: any[];
+  }
 }
 
 export default Recommendations;
--- test/unit/recommendations.ts
+++ test/unit/recommendations.ts
@@ -1,6 +1,86 @@
+import { Events, ProductTransformer, Selectors } from '@storefront/core';
+import * as sinon from 'sinon';
 import Recommendations from '../../src/recommendations';
 import suite from './_suite';
 
-suite('Recommendations', ({ expect, spy }) => {
+const STRUCTURE = { w: 'x' };
+const STATE = { y: 'z' };
 
+suite('Recommendations', ({ expect, spy, stub }) => {
+  let getState: sinon.SinonSpy;
+  let recommendationsProducts: sinon.SinonStub;
+  let recommendations: Recommendations;
+
+  beforeEach(() => {
+    getState = spy();
+    Recommendations.prototype.config = <any>{ structure: STRUCTURE };
+    Recommendations.prototype.flux = <any>{ store: { getState } };
+    recommendationsProducts = stub(Selectors, 'recommendationsProducts').returns([]);
+    recommendations = new Recommendations();
+  });
+  afterEach(() => {
+    delete Recommendations.prototype.flux;
+    delete Recommendations.prototype.config;
+  });
+
+  describe('constructor()', () => {
+    describe('structure', () => {
+      it('should have initial value', () => {
+        expect(recommendations.structure).to.eq(STRUCTURE);
+      });
+    });
+
+    describe('state', () => {
+      it('should set initial value', () => {
+        const products = ['a', 'b', 'c'];
+        const remapped = ['d', 'e', 'f'];
+        const mapProducts = stub(Recommendations.prototype, 'mapProducts').returns(remapped);
+        recommendationsProducts.returns(products);
+
+        const tag = new Recommendations();
+
+        expect(tag.state).to.eql({ products: remapped });
+        expect(mapProducts).to.be.calledWith(products);
+      });
+    });
+  });
+
+  describe('init()', () => {
+    it('should listen for RECOMMENDATIONS_PRODUCTS_UPDATED', () => {
+      const on = spy();
+      recommendations.flux = <any>{ on };
+
+      recommendations.init();
+
+      expect(on).to.be.calledWithExactly(Events.RECOMMENDATIONS_PRODUCTS_UPDATED, recommendations.updateProducts);
+    });
+  });
+
+  describe('updateProducts()', () => {
+    it('should set products', () => {
+      const products: any[] = ['a', 'b', 'c'];
+      const remapped = ['d', 'e', 'f'];
+      const set = recommendations.set = spy();
+      const mapProducts = recommendations.mapProducts = spy(() => remapped);
+
+      recommendations.updateProducts(products);
+
+      expect(set).to.be.calledWithExactly({ products: remapped });
+      expect(mapProducts).to.be.calledWithExactly(products);
+    });
+  });
+
+  describe('mapProducts()', () => {
+    it('should transform and remap products', () => {
+      const transform = spy(() => 'x');
+      const transformer = stub(ProductTransformer, 'transformer').returns(transform);
+
+      expect(recommendations.mapProducts(<any[]>['a', 'b', 'c'])).to.eql(['x', 'x', 'x']);
+      expect(transformer).to.be.calledWithExactly(STRUCTURE);
+      expect(transform).to.be.calledThrice
+        .and.calledWith('a')
+        .and.calledWith('b')
+        .and.calledWith('c');
+    });
+  });
 });
