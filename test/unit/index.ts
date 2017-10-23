84b39a70547680c9f5afd511240b147255c64769
Bruce Chen <bruce.chen@groupbyinc.com>
2017-10-23 10:30:40 -0400

    Add tests for past-purchases

--- test/unit/index.ts
+++ test/unit/index.ts
@@ -1,9 +1,14 @@
 import * as pkg from '../../src';
+import PastPurchases from '../../src/past-purchases';
 import Recommendations from '../../src/recommendations';
 import suite from './_suite';
 
 suite('package', ({ expect }) => {
   it('should expose Recommendations', () => {
     expect(pkg.Recommendations).to.eq(Recommendations);
+  });
+
+  it('should expose PastPurchases', () => {
+    expect(pkg.PastPurchases).to.eq(PastPurchases);
   });
 });
