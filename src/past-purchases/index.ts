9a455a9514d2a107781c73eaf8c3f571ce2b8214
Bruce Chen <bruce.chen@groupbyinc.com>
2017-11-06 12:17:42 -0500

    Change component to be similar to gb-products

--- src/past-purchases/index.ts
+++ src/past-purchases/index.ts
@@ -1,24 +1,39 @@
-import { alias, tag, Events, ProductTransformer, Selectors, Store, Structure, Tag } from '@storefront/core';
+import {
+  alias,
+  configurable,
+  tag,
+  Events,
+  ProductTransformer,
+  Selectors,
+  Store,
+  Structure,
+  Tag
+} from '@storefront/core';
 
+export type Transformer = (product: Store.ProductWithMetadata) => { data: object, variants: object[], meta: any };
+
+@configurable
 @alias('pastPurchases')
 @tag('gb-past-purchases', require('./index.html'))
 class PastPurchases {
 
   structure: Structure = this.config.structure;
   state: PastPurchases.State = {
-    products: this.mapProducts(Selectors.orderHistory(this.flux.store.getState()))
+    products: []
   };
 
+  productTransformer: Transformer = ({ data, meta }: Store.ProductWithMetadata) =>
+    ({ ...ProductTransformer.transformer(this.structure)(data), meta })
+
   init() {
-    this.flux.actions.fetchOrderHistory();
     this.flux.on(Events.ORDER_HISTORY_UPDATED, this.updateProducts);
   }
 
-  updateProducts = (products: Store.Product[]) =>
+  updateProducts = (products: Store.ProductWithMetadata[]) =>
     this.set({ products: this.mapProducts(products) })
 
-  mapProducts(products: Store.Product[]) {
-   return products.map(ProductTransformer.transformer(this.structure));
+  mapProducts(products: Store.ProductWithMetadata[]) {
+   return products.map(this.productTransformer);
   }
 }
 
