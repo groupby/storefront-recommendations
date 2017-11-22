c25d7b738232693a5e960815a5d62b487ef3e212
Filip Francetic <f.francet@hotmail.com>
2017-11-22 14:43:40 -0500

    Incorrect import

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,13 @@
 import PastPurchases from './past-purchases';
-import PastPurchasesPageSize from './past-purchases-page-size';
-import PastPurchasesPaging from './past-purchases-paging';
 import PastPurchasesPill from './past-purchases-pill';
 import PastPurchasesPills from './past-purchases-pills';
 import PastPurchasesSort from './past-purchases-sort';
 import Recommendations from './recommendations';
 
 export {
-    PastPurchases,
-    PastPurchasesPageSize,
-    PastPurchasesPaging,
-    PastPurchasesPill,
-    PastPurchasesPills,
-    PastPurchasesSort,
-    Recommendations,
+  PastPurchases,
+  PastPurchasesPill,
+  PastPurchasesPills,
+  PastPurchasesSort,
+  Recommendations,
 };
